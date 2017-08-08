b3f4445e278e7b15179ab5bdce47b246d03bceb4
pydeveloperKirito <30815286+pydeveloperKirito@users.noreply.github.com>
2017-08-08 03:29:34 +0200

    Create Client.py

---
+++ Client.py
@@ -0,0 +1,38 @@
+import time
+import random
+import datetime
+import telepot.aio
+from telepot.aio.loop import MessageLoop
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, ForceReply
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+from telepot.namedtuple import InlineQueryResultArticle, InlineQueryResultPhoto, InputTextMessageContent
+import telepot
+from telepot.loop import MessageLoop
+bot = telepot.Bot('inserisci il tuo token qui')
+def message ():
+    testo = input("cosa vuoi inviare?: ")
+    if testo != "": 
+       bot.sendMessage(chat,testo)
+def choice():
+    print ("digita ciao123 per uscire")
+    print ("digita text per inviare un testo")
+    print ("digita button per inviare un bottone")
+chat = input("in quale chat vuoi inviare il messaggio?: ")
+choice ()
+scelta = input ("cosa vuoi fare?: ")
+if scelta == "text":
+    message ()
+    choice()
+    scelta = input()
+    if scelta == "ciao123":
+       scelta = input()
+elif scelta == "button":
+    testobottone = input("digita il testo del bottone: ")
+    urlbottone = input("digita l'url del bottone: ")
+    testo = input("digita il testo principale del messaggio: ")
+    markup = InlineKeyboardMarkup(inline_keyboard=[
+    [dict(text= testobottone, url=urlbottone)]
+    ])
+    bot.sendMessage(chat,testo,reply_markup=markup )
+    choice ()
+    scelta = input()
